506005603360f1956e8d90b2127d05d73cf6487f
Wojciech Szalkiewicz <wojciechszalk97@gmail.com>
2022-10-04 21:56:31 +0200

    Complete script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 function titleClickHandler(event){
     console.log('Link was clicked!');
+    event.preventDefault();
     const clickedElement = this;
   
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -24,10 +25,15 @@ for(let activeLink of activeLinks){
     }
   
     /* get 'href' attribute from the clicked link */
-  
+
+    const takenAttribute = clickedElement.getAttribute('href')
+    console.log(takenAttribute)
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(takenAttribute)
+    console.log(targetArticle)
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active')
+    
   }
   
   const links = document.querySelectorAll('.titles a');
